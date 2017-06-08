ab15e55b905aa9aa513af7245f6e3677c273d68b
Ninja-007 <vikas.pisal@gmail.com>
2017-06-08 20:06:17 +0300

    ostream operators added
    ostream operators added

--- include/sqlite_orm/sqlite_orm.h
+++ include/sqlite_orm/sqlite_orm.h
@@ -2254,14 +2254,26 @@ namespace sqlite_orm {
     };
     
     
-enum class sync_schema_result {
-    new_table_created,  /*created new table, table with same tablename did not exist.*/
-    already_in_sync,  /*table schema is same as storage, nothing to be done*/
-    old_columns_removed, /*removed excess columns in table(than storage) without dropping table*/
-    new_columns_added, /*lacking columns in table(than storage) added without dropping table*/
-    new_columns_added_and_old_columns_removed, /*both old_columns_removed and new_columns_added*/
-    dropped_and_recreated /*old table is dropped and new recreated. Reasons 1. delete excess columns in table than storage if preseve=false 2. Lacking columns in table cannot be added due to NULL and default constraint, 3. Reasons 1 and 2 both together. 4. data_type mismatch between table and storage. */
-};
+    enum class sync_schema_result {
+        new_table_created,  /*created new table, table with same tablename did not exist.*/
+        already_in_sync,  /*table schema is same as storage, nothing to be done*/
+        old_columns_removed, /*removed excess columns in table(than storage) without dropping table*/
+        new_columns_added, /*lacking columns in table(than storage) added without dropping table*/
+        new_columns_added_and_old_columns_removed, /*both old_columns_removed and new_columns_added*/
+        dropped_and_recreated /*old table is dropped and new recreated. Reasons 1. delete excess columns in table than storage if preseve=false 2. Lacking columns in table cannot be added due to NULL and default constraint, 3. Reasons 1 and 2 both together. 4. data_type mismatch between table and storage. */
+    };
+
+
+    inline std::ostream& operator<<(std::ostream &os, sync_schema_result value) {
+        switch(value){
+            case sync_schema_result::new_table_created: return os << "new table created";
+            case sync_schema_result::already_in_sync: return os << "table and storage is already in sync.";
+            case sync_schema_result::old_columns_removed: return os << "old excess columns removed";
+            case sync_schema_result::new_columns_added: return os << "new columns added";
+            case sync_schema_result::new_columns_added_and_old_columns_removed: return os << "old excess columns removed and new columns added";
+            case sync_schema_result::dropped_and_recreated: return os << "old table dropped and recreated";
+        }
+    }
 
     template<class ...Ts>
     struct storage_impl {
