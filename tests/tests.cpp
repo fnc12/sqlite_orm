63f59330830488c8ae4319b4abcc9bce1ef3e072
fnc12 <fnc12345@gmail.com>
2018-07-13 20:17:55 +0300

    added SQLITE_ORM_OMITS_CODECVT macro

--- tests/tests.cpp
+++ tests/tests.cpp
@@ -2057,6 +2057,7 @@ void testBusyTimeout() {
     storage.busy_timeout(500);
 }
 
+#ifndef SQLITE_ORM_OMITS_CODECVT
 void testWideString() {
     cout << __func__ << endl;
     
@@ -2087,6 +2088,7 @@ void testWideString() {
     }
     
 }
+#endif  //  SQLITE_ORM_OMITS_CODECVT
 
 void testRowId() {
     cout << __func__ << endl;
@@ -2170,7 +2172,9 @@ int main(int argc, char **argv) {
     
     testBusyTimeout();
     
+#ifndef SQLITE_ORM_OMITS_CODECVT
     testWideString();
+#endif  //  SQLITE_ORM_OMITS_CODECVT
     
     testIssue86();
     
