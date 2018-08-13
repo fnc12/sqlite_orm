d0291d618c56890fab4daeaa4636ddbbe40f9323
fnc12 <fnc12345@gmail.com>
2018-08-13 19:58:27 +0300

    added having with example

--- dev/conditions.h
+++ dev/conditions.h
@@ -18,7 +18,7 @@ namespace sqlite_orm {
             bool offset_is_implicit = false;
             int off = 0;
             
-            limit_t(){}
+            limit_t() = default;
             
             limit_t(decltype(lim) lim_): lim(lim_) {}
             
@@ -87,7 +87,7 @@ namespace sqlite_orm {
         struct negated_condition_t : public condition_t {
             C c;
             
-            negated_condition_t(){}
+            negated_condition_t() = default;
             
             negated_condition_t(C c_): c(c_) {}
             
@@ -104,7 +104,7 @@ namespace sqlite_orm {
             L l;
             R r;
             
-            and_condition_t(){}
+            and_condition_t() = default;
             
             and_condition_t(L l_, R r_): l(l_), r(r_) {}
             
@@ -121,7 +121,7 @@ namespace sqlite_orm {
             L l;
             R r;
             
-            or_condition_t(){}
+            or_condition_t() = default;
             
             or_condition_t(L l_, R r_): l(l_), r(r_) {}
             
@@ -138,7 +138,7 @@ namespace sqlite_orm {
             L l;
             R r;
             
-            binary_condition(){}
+            binary_condition() = default;
             
             binary_condition(L l_, R r_): l(l_), r(r_) {}
         };
@@ -328,6 +328,9 @@ namespace sqlite_orm {
             }
         };
         
+        /**
+         *  IN operator object.
+         */
         template<class L, class A>
         struct in_t : public condition_t {
             using self = in_t<L, A>;
@@ -353,6 +356,9 @@ namespace sqlite_orm {
             }
         };
         
+        /**
+         *  IS NULL operator object.
+         */
         template<class T>
         struct is_null_t {
             using self = is_null_t<T>;
@@ -367,6 +373,9 @@ namespace sqlite_orm {
             }
         };
         
+        /**
+         *  IS NOT NULL operator object.
+         */
         template<class T>
         struct is_not_null_t {
             using self = is_not_null_t<T>;
@@ -382,6 +391,9 @@ namespace sqlite_orm {
             }
         };
         
+        /**
+         *  WHERE argument holder.
+         */
         template<class C>
         struct where_t {
             C c;
@@ -391,6 +403,9 @@ namespace sqlite_orm {
             }
         };
         
+        /**
+         *  ORDER BY argument holder.
+         */
         template<class O>
         struct order_by_t {
             using self = order_by_t<O>;
@@ -444,6 +459,9 @@ namespace sqlite_orm {
             }
         };
         
+        /**
+         *  ORDER BY pack holder.
+         */
         template<class ...Args>
         struct multi_order_by_t {
             std::tuple<Args...> args;
@@ -453,6 +471,9 @@ namespace sqlite_orm {
             }
         };
         
+        /**
+         *  GROUP BY pack holder.
+         */
         template<class ...Args>
         struct group_by_t {
             std::tuple<Args...> args;
@@ -462,6 +483,9 @@ namespace sqlite_orm {
             }
         };
         
+        /**
+         *  BETWEEN operator object.
+         */
         template<class A, class T>
         struct between_t : public condition_t {
             A expr;
@@ -477,12 +501,15 @@ namespace sqlite_orm {
             }
         };
         
+        /**
+         *  LIKE operator object.
+         */
         template<class A, class T>
         struct like_t : public condition_t {
             A a;
             T t;
             
-            like_t(){}
+            like_t() = default;
             
             like_t(A a_, T t_): a(a_), t(t_) {}
             
@@ -491,6 +518,10 @@ namespace sqlite_orm {
             }
         };
         
+        /**
+         *  CROSS JOIN holder.
+         *  T is joined type which represents any mapped table.
+         */
         template<class T>
         struct cross_join_t {
             using type = T;
@@ -500,6 +531,10 @@ namespace sqlite_orm {
             }
         };
         
+        /**
+         *  NATURAL JOIN holder.
+         *  T is joined type which represents any mapped table.
+         */
         template<class T>
         struct natural_join_t {
             using type = T;
@@ -509,6 +544,11 @@ namespace sqlite_orm {
             }
         };
         
+        /**
+         *  LEFT JOIN holder.
+         *  T is joined type which represents any mapped table.
+         *  O is on(...) argument type.
+         */
         template<class T, class O>
         struct left_join_t {
             using type = T;
@@ -521,6 +561,11 @@ namespace sqlite_orm {
             }
         };
         
+        /**
+         *  Simple JOIN holder.
+         *  T is joined type which represents any mapped table.
+         *  O is on(...) argument type.
+         */
         template<class T, class O>
         struct join_t {
             using type = T;
@@ -533,6 +578,11 @@ namespace sqlite_orm {
             }
         };
         
+        /**
+         *  LEFT OUTER JOIN holder.
+         *  T is joined type which represents any mapped table.
+         *  O is on(...) argument type.
+         */
         template<class T, class O>
         struct left_outer_join_t {
             using type = T;
@@ -545,15 +595,24 @@ namespace sqlite_orm {
             }
         };
         
+        /**
+         *  on(...) argument holder used for JOIN, LEFT JOIN, LEFT OUTER JOIN and INNER JOIN
+         *  T is on type argument.
+         */
         template<class T>
         struct on_t {
-            T t;
+            using type = T;
+            
+            type t;
             
             operator std::string() const {
                 return "ON";
             }
         };
         
+        /**
+         *  USING argument holder.
+         */
         template<class F, class O>
         struct using_t {
             F O::*column;
@@ -563,6 +622,11 @@ namespace sqlite_orm {
             }
         };
         
+        /**
+         *  INNER JOIN holder.
+         *  T is joined type which represents any mapped table.
+         *  O is on(...) argument type.
+         */
         template<class T, class O>
         struct inner_join_t {
             using type = T;
@@ -575,12 +639,22 @@ namespace sqlite_orm {
             }
         };
         
+        template<class T>
+        struct having_t {
+            using type = T;
+            
+            type t;
+            
+            operator std::string() const {
+                return "HAVING";
+            }
+        };
+        
     }
     
     /**
      *  Cute operators for columns
      */
-    
     template<class T, class R>
     conditions::lesser_than_t<T, R> operator<(internal::expression_t<T> expr, R r) {
         return {expr.t, r};
@@ -933,5 +1007,10 @@ namespace sqlite_orm {
     template<class A, class T>
     conditions::like_t<A, T> like(A a, T t) {
         return {a, t};
+    }
+    
+    template<class T>
+    conditions::having_t<T> having(T t) {
+        return {t};
     }
 }
