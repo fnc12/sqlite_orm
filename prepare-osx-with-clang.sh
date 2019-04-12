b1887bed2d551f351665d41dcbc7a5c83d657426
Seyyed Soroosh Hosseinalipour <soorosh_abi@hotmail.com>
2019-04-13 01:40:11 +0430

    Add -AllowAppleClang option to vcpkg. (see #256)

--- prepare-osx-with-clang.sh
+++ prepare-osx-with-clang.sh
@@ -3,7 +3,7 @@
 # exit on firts error
 set -e
 
-brew install --with-toolchain llvm
+brew install llvm
 brew info llvm
 export PATH="/usr/local/opt/llvm/bin:$PATH"
 echo 'export PATH="/usr/local/opt/llvm/bin:$PATH"' >> ~/.bash_profile
