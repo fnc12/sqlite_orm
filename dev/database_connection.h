0c617bdade524779a41ecffc85cfb9d261700318
Auke Bruinsma <github@air2.dds.nl>
2019-11-05 16:10:20 +0100

    Use the new throw function

--- dev/database_connection.h
+++ dev/database_connection.h
@@ -5,26 +5,23 @@
 #include <system_error> //  std::error_code, std::system_error
 
 #include "error_code.h"
-
 namespace sqlite_orm {
     
     namespace internal {
 
         inline sqlite3 *open_db(std::string const&filename)
-    	{
-    		sqlite3 *result {nullptr};
-			if(sqlite3_open(filename.c_str(), &result) != SQLITE_OK){
-				
-				throw std::system_error(std::error_code(sqlite3_errcode(result), get_sqlite_error_category()), get_error_message(result, "opening '", filename, "'. "));
+        {
+            sqlite3 *result {nullptr};
+            if(sqlite3_open(filename.c_str(), &result) != SQLITE_OK){
+                throw_error(result, "opening '", filename, "'. ");
             }
-			return result;
-    	}
+            return result;
+        }
     	
         struct database_connection {
             explicit database_connection(const std::string &filename):
-				db {open_db(filename) }
-    		{
-            }
+               db {open_db(filename) }
+            {}
             
             ~database_connection() {
                 sqlite3_close(this->db);
