e688cca702ec91a80271c686ff52c0902e951a8d
Guillaume Mayer <guillaume_mayer@hotmail.com>
2019-11-09 21:58:48 -0500

    removed unnecessary stuff in is_std_ptr. Those things should have been removed when I rollbacked my trial in field_printer.

--- dev/is_std_ptr.h
+++ dev/is_std_ptr.h
@@ -10,41 +10,29 @@ namespace sqlite_orm {
 
     template<typename T>
     struct is_std_ptr<std::shared_ptr<T>> : std::true_type {
-        using container_type = typename std::shared_ptr<T>;
         using element_type = T;
 
         static std::shared_ptr<T> make(const T &v) {
             return std::make_shared<T>(v);
         }
-        static bool isEmpty(const std::shared_ptr<T> &v) {
-            return static_cast<bool>(v);
-        }
     };
 
     template<typename T>
     struct is_std_ptr<std::unique_ptr<T>> : std::true_type {
-        using container_type = typename std::unique_ptr<T>;
         using element_type = T;
 
         static std::unique_ptr<T> make(const T &v) {
             return std::make_unique<T>(v);
-        }
-        static bool isEmpty(const std::unique_ptr<T> &v) {
-            return static_cast<bool>(v);
         }
     };
 
 #ifdef SQLITE_ORM_OPTIONAL_SUPPORTED
     template<typename T>
     struct is_std_ptr<std::optional<T>> : std::true_type {
-        using container_type = typename std::optional<T>;
         using element_type = T;
 
         static std::optional<T> make(const T &v) {
             return std::make_optional<T>(v);
-        }
-        static bool isEmpty(const std::optional<T> &v) {
-            return v.has_value();
         }
     };
 #endif  // SQLITE_ORM_OPTIONAL_SUPPORTED
