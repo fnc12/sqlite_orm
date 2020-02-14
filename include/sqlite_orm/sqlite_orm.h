9e33008bf61ed90cc1b5f5b0973496fc9d16b346
Cyril LEVACHER <clevacher@eos-imaging.com>
2020-02-14 14:17:49 +0100

    Avoid foreign and/or references to be understand as SQLite command

--- include/sqlite_orm/sqlite_orm.h
+++ include/sqlite_orm/sqlite_orm.h
@@ -8949,7 +8949,7 @@ namespace sqlite_orm {
                 });
                 ss << "FOREIGN KEY( ";
                 for(size_t i = 0; i < columnNames.size(); ++i) {
-                    ss << columnNames[i];
+                    ss << "'" << columnNames[i] << "'";
                     if(i < columnNames.size() - 1) {
                         ss << ",";
                     }
@@ -8971,7 +8971,7 @@ namespace sqlite_orm {
                 });
                 ss << "( ";
                 for(size_t i = 0; i < referencesNames.size(); ++i) {
-                    ss << referencesNames[i];
+                    ss << "'" << referencesNames[i] << "'";
                     if(i < referencesNames.size() - 1) {
                         ss << ",";
                     }
