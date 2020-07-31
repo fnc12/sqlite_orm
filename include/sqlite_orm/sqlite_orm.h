3a543f000f2c8a87620242e03c4866608aa5ad9a
fnc12 <fnc12345@gmail.com>
2020-08-01 01:53:45 +0300

    removed sqlite type and its comparison

--- include/sqlite_orm/sqlite_orm.h
+++ include/sqlite_orm/sqlite_orm.h
@@ -133,92 +133,6 @@ namespace std {
 }
 #pragma once
 
-#include <map>  //  std::map
-#include <string>  //  std::string
-#include <regex>  //  std::regex, std::regex_match
-#include <memory>  //  std::make_unique, std::unique_ptr
-#include <vector>  //  std::vector
-#include <cctype>  //  std::toupper
-
-namespace sqlite_orm {
-    using int64 = sqlite_int64;
-    using uint64 = sqlite_uint64;
-
-    //  numeric and real are the same for c++
-    enum class sqlite_type {
-        INTEGER,
-        TEXT,
-        BLOB,
-        REAL,
-    };
-
-    /**
-     *  @param str case doesn't matter - it is uppercased before comparing.
-     */
-    inline std::unique_ptr<sqlite_type> to_sqlite_type(const std::string &str) {
-        auto asciiStringToUpper = [](std::string &s) {
-            std::transform(s.begin(), s.end(), s.begin(), [](char c) {
-                return static_cast<char>(std::toupper(static_cast<int>(c)));
-            });
-        };
-        auto upperStr = str;
-        asciiStringToUpper(upperStr);
-
-        static std::map<sqlite_type, std::vector<std::regex>> typeMap = {
-            {sqlite_type::INTEGER,
-             {
-                 std::regex("INT"),
-                 std::regex("INT.*"),
-                 std::regex("TINYINT"),
-                 std::regex("SMALLINT"),
-                 std::regex("MEDIUMINT"),
-                 std::regex("BIGINT"),
-                 std::regex("UNSIGNED BIG INT"),
-                 std::regex("INT2"),
-                 std::regex("INT8"),
-                 std::regex("UNSIGNED\\s+INT\\([[:digit:]]+\\)"),
-             }},
-            {sqlite_type::TEXT,
-             {
-                 std::regex("CHARACTER\\([[:digit:]]+\\)"),
-                 std::regex("VARCHAR\\([[:digit:]]+\\)"),
-                 std::regex("VARYING CHARACTER\\([[:digit:]]+\\)"),
-                 std::regex("NCHAR\\([[:digit:]]+\\)"),
-                 std::regex("NATIVE CHARACTER\\([[:digit:]]+\\)"),
-                 std::regex("NVARCHAR\\([[:digit:]]+\\)"),
-                 std::regex("CLOB"),
-                 std::regex("TEXT"),
-             }},
-            {sqlite_type::BLOB,
-             {
-                 std::regex("BLOB"),
-             }},
-            {sqlite_type::REAL,
-             {
-                 std::regex("REAL"),
-                 std::regex("DOUBLE"),
-                 std::regex("DOUBLE PRECISION"),
-                 std::regex("FLOAT"),
-                 std::regex("NUMERIC"),
-                 std::regex("DECIMAL\\([[:digit:]]+,[[:digit:]]+\\)"),
-                 std::regex("BOOLEAN"),
-                 std::regex("DATE"),
-                 std::regex("DATETIME"),
-             }},
-        };
-        for(auto &p: typeMap) {
-            for(auto &r: p.second) {
-                if(std::regex_match(upperStr, r)) {
-                    return std::make_unique<sqlite_type>(p.first);
-                }
-            }
-        }
-
-        return {};
-    }
-}
-#pragma once
-
 #include <tuple>  //  std::tuple, std::get
 #include <type_traits>  //  std::false_type, std::true_type
 
@@ -5673,6 +5587,9 @@ namespace sqlite_orm {
 
 namespace sqlite_orm {
 
+    using int64 = sqlite_int64;
+    using uint64 = sqlite_uint64;
+
     namespace internal {
 
         /**
@@ -6235,8 +6152,6 @@ namespace sqlite_orm {
 
 // #include "sync_schema_result.h"
 
-// #include "sqlite_type.h"
-
 // #include "field_value_holder.h"
 
 #include <type_traits>  //  std::enable_if
@@ -6335,29 +6250,19 @@ namespace sqlite_orm {
                     });
                     if(dbColumnInfoIt != dbTableInfo.end()) {
                         auto &dbColumnInfo = *dbColumnInfoIt;
-                        auto dbColumnInfoType = to_sqlite_type(dbColumnInfo.type);
-                        auto storageColumnInfoType = to_sqlite_type(storageColumnInfo.type);
-                        if(dbColumnInfoType && storageColumnInfoType) {
-                            auto columnsAreEqual =
-                                dbColumnInfo.name == storageColumnInfo.name &&
-                                *dbColumnInfoType == *storageColumnInfoType &&
-                                dbColumnInfo.notnull == storageColumnInfo.notnull &&
-                                (dbColumnInfo.dflt_value.length() > 0) == (storageColumnInfo.dflt_value.length() > 0) &&
-                                dbColumnInfo.pk == storageColumnInfo.pk;
-                            if(!columnsAreEqual) {
-                                notEqual = true;
-                                break;
-                            }
-                            dbTableInfo.erase(dbColumnInfoIt);
-                            storageTableInfo.erase(storageTableInfo.begin() +
-                                                   static_cast<ptrdiff_t>(storageColumnInfoIndex));
-                            --storageColumnInfoIndex;
-                        } else {
-
-                            //  undefined type/types
+                        auto columnsAreEqual =
+                            dbColumnInfo.name == storageColumnInfo.name &&
+                            dbColumnInfo.notnull == storageColumnInfo.notnull &&
+                            (dbColumnInfo.dflt_value.length() > 0) == (storageColumnInfo.dflt_value.length() > 0) &&
+                            dbColumnInfo.pk == storageColumnInfo.pk;
+                        if(!columnsAreEqual) {
                             notEqual = true;
                             break;
                         }
+                        dbTableInfo.erase(dbColumnInfoIt);
+                        storageTableInfo.erase(storageTableInfo.begin() +
+                                               static_cast<ptrdiff_t>(storageColumnInfoIndex));
+                        --storageColumnInfoIndex;
                     } else {
                         columnsToAdd.push_back(&storageColumnInfo);
                     }
