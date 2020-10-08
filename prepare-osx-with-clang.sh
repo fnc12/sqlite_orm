c6c77650c242bcef6f53c6ac29f4b3f2e2a8ef38
Leon De Andrade <leondeandrade@hotmail.com>
2020-10-08 19:11:46 +0200

    Cleanup

--- prepare-osx-with-clang.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-# exit on firts error
-set -e
-
-brew install llvm
-brew info llvm
-export PATH="/usr/local/opt/llvm/bin:$PATH"
-echo 'export PATH="/usr/local/opt/llvm/bin:$PATH"' >> ~/.zshenv
