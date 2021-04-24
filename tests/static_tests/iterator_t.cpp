101a4dae720350d2318cb5adf24570aae39b777e
denzor200 <denismikhaylov38@gmail.com>
2021-04-24 16:34:23 +0400

    Fix for building test in C++14

--- tests/static_tests/iterator_t.cpp
+++ tests/static_tests/iterator_t.cpp
@@ -17,20 +17,20 @@ TEST_CASE("iterator_t") {
     using iter = decltype(std::declval<storage>().iterate<User>().begin());
 
     // weakly_incrementable
-    static_assert(std::is_default_constructible_v<iter>, "needs to be default constructible");
-    static_assert(std::is_same_v<typename iter::difference_type, std::ptrdiff_t>, "needs to have difference_type");
-    static_assert(std::is_same_v<decltype(++std::declval<iter>()), iter&>, "needs to be incrementable");
+    static_assert(std::is_default_constructible<iter>::value, "needs to be default constructible");
+    static_assert(std::is_same<typename iter::difference_type, std::ptrdiff_t>::value, "needs to have difference_type");
+    static_assert(std::is_same<decltype(++std::declval<iter>()), iter&>::value, "needs to be incrementable");
     using check = decltype(std::declval<iter>()++);
 
     // indirectly_readable
-    static_assert(std::is_same_v<decltype(*std::declval<const iter>()), const User&>,
+    static_assert(std::is_same<decltype(*std::declval<const iter>()), const User&>::value,
                   "needs to be const dereferencable");
 
     // input_iterator
-    static_assert(std::is_same_v<typename iter::iterator_category, std::input_iterator_tag>,
+    static_assert(std::is_same<typename iter::iterator_category, std::input_iterator_tag>::value,
                   "needs to have iterator_category");
 
     // sentinel
-    static_assert(std::is_same_v<decltype(std::declval<const iter>() == std::declval<const iter>()), bool>,
+    static_assert(std::is_same<decltype(std::declval<const iter>() == std::declval<const iter>()), bool>::value,
                   "supports equality checking");
 }
