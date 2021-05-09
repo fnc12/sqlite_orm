c9d3a8e5aa60d19cc450ab481b3a3a010f734ff2
Yevgeniy Zakharov <fnc12@me.com>
2021-05-09 19:20:40 +0600

    added ifnull with unit tests

--- include/sqlite_orm/sqlite_orm.h
+++ include/sqlite_orm/sqlite_orm.h
@@ -3915,6 +3915,12 @@ namespace sqlite_orm {
             }
         };
 
+        struct ifnull_string {
+            operator std::string() const {
+                return "IFNULL";
+            }
+        };
+
         struct date_string {
             operator std::string() const {
                 return "DATE";
@@ -4398,6 +4404,14 @@ namespace sqlite_orm {
     template<class R, class... Args>
     internal::core_function_t<R, internal::coalesce_string, Args...> coalesce(Args... args) {
         return {std::make_tuple(std::forward<Args>(args)...)};
+    }
+
+    /**
+     *  IFNULL(X,Y) function https://www.sqlite.org/lang_corefunc.html#ifnull
+     */
+    template<class R, class X, class Y>
+    internal::core_function_t<R, internal::ifnull_string, X, Y> ifnull(X x, Y y) {
+        return {std::make_tuple(std::move(x), std::move(y))};
     }
 
     /**
