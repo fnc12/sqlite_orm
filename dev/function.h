9e44f009fbef7f5b6567c53b52bfc671e3d8f394
Yevgeniy Zakharov <fnc12@me.com>
2021-07-05 01:48:45 +0600

    added shift

--- dev/function.h
+++ dev/function.h
@@ -5,20 +5,20 @@
 
 namespace sqlite_orm {
 
-namespace internal {
+    namespace internal {
 
-struct scalar_function_t {
-    
-    template<class T>
-    scalar_function_t(T callable) {
-        function = [](sqlite3_context *context, int argc, sqlite3_value **argv) {
+        struct scalar_function_t {
             
+            template<class T>
+            scalar_function_t(T callable) {
+                function = [](sqlite3_context *context, int argc, sqlite3_value **argv) {
+                    
+                };
+            }
+            
+            std::function<void(sqlite3_context *context, int argc, sqlite3_value **argv)> function;
         };
-    }
-    
-    std::function<void(sqlite3_context *context, int argc, sqlite3_value **argv)> function;
-};
 
-}
+    }
 
 }
