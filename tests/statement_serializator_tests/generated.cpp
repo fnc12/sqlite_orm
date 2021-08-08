1ddc09c7640725204771a0de393faae4b144e5dc
denzor200 <denismikhaylov38@gmail.com>
2021-08-08 17:36:57 +0400

    clang-format

--- tests/statement_serializator_tests/generated.cpp
+++ tests/statement_serializator_tests/generated.cpp
@@ -3,44 +3,37 @@
 
 using namespace sqlite_orm;
 
-TEST_CASE("statement_serializator generated")
-{
+TEST_CASE("statement_serializator generated") {
     using storage_impl_t = internal::storage_impl<>;
     storage_impl_t storageImpl;
     internal::serializator_context<storage_impl_t> context{storageImpl};
-    SECTION("generated_always_as")
-    {
+    SECTION("generated_always_as") {
         auto statement = generated_always().as(1);
         auto stringValue = internal::serialize(statement, context);
         REQUIRE(stringValue == "GENERATED ALWAYS AS(1)");
 
-        SECTION("VIRTUAL")
-        {
+        SECTION("VIRTUAL") {
             auto statementVirtual = statement.virtual_();
             stringValue = internal::serialize(statementVirtual, context);
             REQUIRE(stringValue == "GENERATED ALWAYS AS(1) VIRTUAL");
         }
-        SECTION("STORED")
-        {
+        SECTION("STORED") {
             auto statementStored = statement.stored();
             stringValue = internal::serialize(statementStored, context);
             REQUIRE(stringValue == "GENERATED ALWAYS AS(1) STORED");
         }
     }
-    SECTION("as")
-    {
+    SECTION("as") {
         auto statement = as(1);
         auto stringValue = internal::serialize(statement, context);
         REQUIRE(stringValue == "AS(1)");
 
-        SECTION("VIRTUAL")
-        {
+        SECTION("VIRTUAL") {
             auto statementVirtual = statement.virtual_();
             stringValue = internal::serialize(statementVirtual, context);
             REQUIRE(stringValue == "AS(1) VIRTUAL");
         }
-        SECTION("STORED")
-        {
+        SECTION("STORED") {
             auto statementStored = statement.stored();
             stringValue = internal::serialize(statementStored, context);
             REQUIRE(stringValue == "AS(1) STORED");
