fe5ba659f96a49d8bfd70e073b0f23dac1d260e0
klaus triendl <klaus@triendl.eu>
2022-01-19 19:22:38 +0200

    Simplified and described storage lookup methods

--- dev/storage_lookup.h
+++ dev/storage_lookup.h
@@ -31,26 +31,32 @@ namespace sqlite_orm {
         struct is_storage_impl<const storage_impl<Ts...>> : std::true_type {};
 
         /**
-         *  A data type's CTE alias, otherwise T itself is used as a CTE alias.
+         *  A data type's CTE label, otherwise T itself is used as a CTE alias.
+         * 
+         *  Note: This is useful if operating on a storage implementation pack,
+         *  which already contains a mapping for a label.
          */
         template<typename T>
         using detected_cte_label_t = polyfill::detected_or_t<T, cte_label_type_t, T>;
 
         /**
-         *  std::true_type if given object is mapped, std::false_type otherwise
+         *  std::true_type if given object is mapped, std::false_type otherwise.
          */
         template<typename S, typename O>
         using object_type_matches = typename polyfill::conjunction<std::is_void<storage_cte_label_type_t<S>>,
                                                                    std::is_same<O, storage_object_type_t<S>>>::type;
 
         /**
-         *  std::true_type if object is mapped via given (CTE) label, std::false_type otherwise
+         *  std::true_type if given label is mapped, std::false_type otherwise
          */
         template<typename S, typename Label>
         using cte_label_type_matches = typename polyfill::conjunction<
             std::negation<std::is_void<storage_cte_label_type_t<S>>>,
             std::is_same<detected_cte_label_t<Label>, storage_cte_label_type_t<S>>>::type;
 
+        /**
+         *  std::true_type if given lookup type (object or label) is mapped, std::false_type otherwise.
+         */
         template<typename S, typename Lookup>
         using lookup_type_matches =
             typename polyfill::disjunction<object_type_matches<S, Lookup>, cte_label_type_matches<S, Lookup>>::type;
@@ -122,43 +128,55 @@ namespace sqlite_orm {
 
     namespace internal {
 
+        template<typename T, typename U>
+        using reapply_const_of_t = std::conditional_t<std::is_const<T>::value, std::add_const_t<U>, U>;
+
         /**
-         *  S - storage_t or storage_impl type
+         *  S - storage_t or storage_impl type, possibly const-qualified
          *  Lookup - mapped data type or CTE label
          */
         template<class S, class Lookup>
         using storage_pick_impl_t =
-            std::conditional_t<std::is_const<S>::value,
-                               const typename storage_pick_impl_type<std::remove_const_t<S>, Lookup>::type,
-                               // note: need to remove const on this branch, too, due to SFINAE
-                               typename storage_pick_impl_type<std::remove_const_t<S>, Lookup>::type>;
+            reapply_const_of_t<S, type_t<storage_pick_impl_type<std::remove_const_t<S>, Lookup>>>;
 
         /**
-         *  S - storage_t or storage_impl type
+         *  S - storage_t or storage_impl type, possibly const-qualified
          *  Lookup - mapped data type or CTE label
          */
         template<class S, class Lookup>
         using storage_find_impl_t =
-            std::conditional_t<std::is_const<S>::value,
-                               const typename storage_find_impl_type<std::remove_const_t<S>, Lookup>::type,
-                               // note: need to remove const on this branch, too, due to SFINAE
-                               typename storage_find_impl_type<std::remove_const_t<S>, Lookup>::type>;
+            reapply_const_of_t<S, type_t<storage_find_impl_type<std::remove_const_t<S>, Lookup>>>;
     }
 }
 
 // runtime lookup functions
 namespace sqlite_orm {
     namespace internal {
+        /**
+         *  Given a storage implementation pack, pick the specific storage implementation for the given lookup type.
+         * 
+         *  Note: This function requires Lookup to be mapped, otherwise it is removed from the overload resolution set.
+         */
         template<class Lookup, class S, satisfies<is_storage_impl, S> = true>
         storage_pick_impl_t<S, Lookup>& pick_impl(S& impl) {
             return impl;
         }
 
+        /**
+         *  Given a storage implementation pack, find the specific storage implementation for the given lookup type.
+         * 
+         *  Note: This function returns the empty `storage_impl<>` if Lookup isn't mapped.
+         */
         template<class Lookup, class S, satisfies<is_storage_impl, S> = true>
         storage_find_impl_t<S, Lookup>& find_impl(S& impl) {
             return impl;
         }
 
+        /**
+         *  Given a storage, pick the specific const-qualified storage implementation for the lookup type.
+         * 
+         *  Note: This function requires Lookup to be mapped, otherwise it is removed from the overload resolution set.
+         */
         template<class Lookup, class S, satisfies<is_storage, S> = true>
         storage_pick_impl_t<const S, Lookup>& pick_const_impl(S& storage) {
             return obtain_const_impl(storage);
