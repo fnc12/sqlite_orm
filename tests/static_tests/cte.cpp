93ecf5581e280984679e70fcbb3c1589c455ba49
klaus triendl <klaus@triendl.eu>
2022-04-14 17:25:16 +0300

    Unit tests for explicit CTE column references

---
+++ tests/static_tests/cte.cpp
@@ -0,0 +1,38 @@
+#include <type_traits>  //  std::is_same, std::is_constructible
+#include <sqlite_orm/sqlite_orm.h>
+#include <catch2/catch.hpp>
+
+using namespace sqlite_orm;
+using internal::alias_holder;
+using internal::column_alias;
+using internal::column_t;
+using std::tuple;
+
+template<class ColRefs, class E>
+void do_assert() {
+    STATIC_REQUIRE(std::is_same<ColRefs, E>::value);
+}
+
+template<class E, class ColRef>
+void runTest(ColRef colRef) {
+    do_assert<internal::transform_tuple_t<tuple<ColRef>, internal::decay_explicit_column>, E>();
+}
+
+TEST_CASE("CTE type traits") {
+    SECTION("decay_explicit_column") {
+        struct Org {
+            int64 id = 0;
+        };
+
+        STATIC_REQUIRE(std::is_constructible<alias_holder<column_alias<'c'>>, column_alias<'c'>>::value);
+        runTest<tuple<decltype(&Org::id)>>(&Org::id);
+        runTest<tuple<alias_holder<column_alias<'1'>>>>(1_colalias);
+#if __cpp_nontype_template_args >= 201911
+        runTest<tuple<alias_holder<column_alias<'c'>>>>("a"_col);
+#endif
+        runTest<tuple<column_t<Org, int64, const int64& (Org::*)() const, void (Org::*)(int64)>>>(
+            make_column("id", &Org::id));
+        runTest<tuple<polyfill::remove_cvref_t<decltype(std::ignore)>>>(std::ignore);
+        runTest<tuple<std::string>>("");
+    }
+}
