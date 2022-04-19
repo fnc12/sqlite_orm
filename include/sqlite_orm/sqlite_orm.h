0eb0f4c7d3448b31ed6412100ed654cf7f94587b
klaus triendl <klaus@triendl.eu>
2022-04-19 18:57:07 +0300

    Updated the way feature macros are determined
    * <ciso646> was officially removed in C++, hence include the C-header equivalent <iso646.h> in order to pull in alternative operator representations (also see issue #166)
    * Rely on C++ feature test macros to determine availability of features instead of C++ standard version

--- include/sqlite_orm/sqlite_orm.h
+++ include/sqlite_orm/sqlite_orm.h
@@ -7,25 +7,38 @@ __pragma(push_macro("max"))
 #undef max
 #endif  // defined(_MSC_VER)
 
-#include <ciso646>  //  due to #166
+#include <iso646.h>  //  alternative operator representations
 
-#if __cplusplus >= 201703L  // use of C++17 or higher
-// Enables use of std::optional in SQLITE_ORM.
-#define SQLITE_ORM_OPTIONAL_SUPPORTED
-#define SQLITE_ORM_STRING_VIEW_SUPPORTED
+#if __cpp_noexcept_function_type >= 201510L
 #define SQLITE_ORM_NOTHROW_ALIASES_SUPPORTED
-#define SQLITE_ORM_INLINE_VAR inline
-#if __cplusplus >= 202002L  // use of C++20 or higher
-#define SQLITE_ORM_CONSTEVAL consteval
-#define SQLITE_ORM_NOUNIQUEADDRESS [[no_unique_address]]
-#else
-#define SQLITE_ORM_CONSTEVAL constexpr
-#define SQLITE_ORM_NOUNIQUEADDRESS
 #endif
+
+#if __cpp_inline_variables >= 201606L
+#define SQLITE_ORM_INLINE_VAR inline
 #else
 #define SQLITE_ORM_INLINE_VAR
-#define SQLITE_ORM_CONSTEVAL constexpr
+#endif
+
+#if __has_cpp_attribute(no_unique_address) >= 201803L
+#define SQLITE_ORM_NOUNIQUEADDRESS [[no_unique_address]]
+#else
 #define SQLITE_ORM_NOUNIQUEADDRESS
+#endif
+
+#if __cpp_consteval >= 201811L
+#define SQLITE_ORM_CONSTEVAL consteval
+#else
+#define SQLITE_ORM_CONSTEVAL constexpr
+#endif
+
+#if __cplusplus >= 201703L  // C++17 or later
+#if __has_include(<optional>)
+#define SQLITE_ORM_OPTIONAL_SUPPORTED
+#endif
+
+#if __has_include(<string_view>)
+#define SQLITE_ORM_STRING_VIEW_SUPPORTED
+#endif
 #endif
 #pragma once
 
