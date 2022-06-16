0e4ab850333c4824156cb0e7e256e4d34353ee9d
JUAN DENT <juandent@mac.com>
2022-06-16 10:10:12 -0600

    More complete chrono_binding.cpp example showing some features of chrono namespace to see why it is attractive

--- examples/chrono_binding.cpp
+++ examples/chrono_binding.cpp
@@ -32,16 +32,42 @@ int main(int argc, const char* argv[]) {
                                            make_column("birthdate", &Person::birthdate)));
 
     using namespace std::chrono;
+    using namespace std::chrono_literals;
 
     year_month_day birthdate{year{1960}, month{7}, day{26}};
+    year_month_day or_birthdate{ 1960y, July, 26d };
+    assert(birthdate == or_birthdate);
+
+    sys_days birthdate_tp = birthdate;  // auto conversion to time_point of days units
 
     storage.sync_schema();
 
-    Person person{1, "Juan Dent", birthdate};
+    Person person{1, "Juan Dent", birthdate};   // we are using the implicit operator from year_month_day to sys_days
     storage.replace(person);
 
     auto pers = storage.get<Person>(1);
     year_month_day ymd = pers.birthdate;
+
+    {
+        constexpr sys_days thursday = year_month_weekday{ year{2022} / March / Thursday[2] };
+        constexpr year_month_day second_thursday = thursday;
+        auto diff = (thursday - birthdate_tp).count();
+        std::cout << "Number of days " << diff << " ";
+        auto year_diff = duration_cast<years>(thursday - birthdate_tp).count();
+        std::cout << "Number of years " << year_diff << std::endl;
+        std::ignore = thursday;
+    }
+    {
+        constexpr sys_days thursday = year_month_weekday{ year{2022} / March / Thursday[last] };
+        constexpr year_month_day last_thursday = thursday;
+        std::ignore = last_thursday;
+    }
+
+    {
+        constexpr sys_days monday = year_month_weekday_last{ 2022y, month{March}, weekday_last{Monday} };
+        constexpr year_month_day last_monday = monday;
+        std::ignore = monday;
+    }
 
     assert(ymd == birthdate);
 }
