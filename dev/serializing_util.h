4595779c430f1d6a2ce736e9e2d74051fade0a03
Yevgeniy Zakharov <fnc12@me.com>
2023-10-03 19:00:38 +0600

    refactored serialize declaration

--- dev/serializing_util.h
+++ dev/serializing_util.h
@@ -20,8 +20,8 @@ namespace sqlite_orm {
         template<class O>
         struct order_by_t;
 
-        template<class T, class DBOs>
-        std::string serialize(const T&, const serializer_context<DBOs>&);
+        template<class T, class C>
+        std::string serialize(const T& t, const C& context);
 
         template<class T, class Ctx>
         std::string serialize_order_by(const T&, const Ctx&);
