5d2a8d82b67dd1ce72a6d2a091590afd2685ece1
klaus triendl <klaus@triendl.eu>
2023-10-14 21:57:55 +0200

    Simplified a few node expressions involving comparisons with aliases

--- tests/ast_iterator_tests.cpp
+++ tests/ast_iterator_tests.cpp
@@ -192,7 +192,7 @@ TEST_CASE("ast_iterator") {
             iterate_ast(node, lambda);
         }
         SECTION("column alias in expression") {
-            auto node = order_by(get<colalias_a>() > c(1));
+            auto node = order_by(get<colalias_a>() > 1);
             expected.push_back(typeid(int));
             iterate_ast(node, lambda);
         }
@@ -284,14 +284,14 @@ TEST_CASE("ast_iterator") {
     }
     SECTION("aliases") {
         SECTION("holder") {
-            auto expression = get<colalias_a>() > c(0);
+            auto expression = get<colalias_a>() > 0;
             expected.push_back(typeid(int));
             iterate_ast(expression, lambda);
         }
 #ifdef SQLITE_ORM_WITH_CPP20_ALIASES
         {
             SECTION("direct") {
-                auto expression = "a"_col > c(0);
+                auto expression = "a"_col > 0;
                 expected.push_back(typeid(int));
                 iterate_ast(expression, lambda);
             }
