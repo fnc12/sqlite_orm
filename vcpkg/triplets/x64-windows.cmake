4a60e5a791a79e0a653da959eb982fde21fe4219
klaus triendl <klaus@triendl.eu>
2024-05-18 23:12:08 +0300

    appveyor: vcpkg dependencies only in the “release” build type

--- vcpkg/triplets/x64-windows.cmake
+++ vcpkg/triplets/x64-windows.cmake
@@ -1,10 +1,6 @@
-# x64-linux-dynamic-release
+# x64-windows-dynamic-release
 
 set(VCPKG_TARGET_ARCHITECTURE x64)
 set(VCPKG_CRT_LINKAGE dynamic)
 set(VCPKG_LIBRARY_LINKAGE dynamic)
-
-set(VCPKG_CMAKE_SYSTEM_NAME Linux)
 set(VCPKG_BUILD_TYPE release)
-
-set(VCPKG_FIXUP_ELF_RPATH ON)
