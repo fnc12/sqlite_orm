fb7f62b896183f756aa61645f476576c3e7a1050
klaus triendl <klaus@triendl.eu>
2024-07-20 20:18:10 +0200

    Removed irrelevant comment in `cxx_universal.h`

--- include/sqlite_orm/sqlite_orm.h
+++ include/sqlite_orm/sqlite_orm.h
@@ -19,7 +19,6 @@ __pragma(push_macro("max"))
  *  - ::size_t, ::ptrdiff_t, ::nullptr_t
  *  - C++ core language feature macros
  *  - macros for dealing with compiler quirks
- *  - macros for exporting symbols from the C++ named module
  */
 
 #include <iso646.h>  //  alternative operator representations
