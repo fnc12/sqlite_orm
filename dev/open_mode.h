1d26089fa3b569767fbade5782647c7282d6bd67
Jackson Kaplan <JwyattK@gmail.com>
2025-02-03 09:26:53 -0800

    add open_mode.h

---
+++ dev/open_mode.h
@@ -0,0 +1,25 @@
+#include <sqlite3.h>
+
+#pragma once
+
+namespace sqlite_orm {
+    enum class open_mode_t {
+        default_mode = 0,
+        create_readwrite = 0,
+        create_readonly = 1,
+    };
+
+    namespace internal {
+        inline const int to_int_flags(open_mode_t open_mode) {
+
+            switch (open_mode) {
+                case open_mode_t::create_readonly:
+                    return SQLITE_OPEN_CREATE | SQLITE_OPEN_READONLY;
+                default:
+                case open_mode_t::create_readwrite:
+                    return SQLITE_OPEN_CREATE | SQLITE_OPEN_READWRITE;
+            };
+        }
+    }
+
+}
