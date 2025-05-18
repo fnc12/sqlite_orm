7a2bd15cf804a92ec73d83fd37891ca094d651b3
Yevgeniy Zakharov <fnc12@me.com>
2025-05-18 11:54:15 +0500

    reverted almost the whole concept

--- tests/static_tests/iterator_t.cpp
+++ tests/static_tests/iterator_t.cpp
@@ -52,6 +52,15 @@ concept LegacyInputIterator =
         requires std::signed_integral<typename std::incrementable_traits<Iter>::difference_type>;
     };
 
+template<class Iter, class Value>
+concept can_iterate_mapped = requires(Iter it) {
+    requires LegacyInputIterator<Iter>;
+    // explicit check of the sentinel role, since `std::ranges::borrowed_range` in `can_view_mapped`
+    // would not tell us why exactly the end iterator cannot be a sentinel
+    requires std::sentinel_for<Iter, Iter>;
+    { *it } -> std::same_as<Value&>;
+};
+
 template<class V, class O, class DBOs>
 concept can_view_mapped = requires(V view) {
     requires std::ranges::borrowed_range<V>;
@@ -104,6 +113,9 @@ TEST_CASE("can view and iterate mapped") {
 
 #ifdef SQLITE_ORM_CPP20_CONCEPTS_SUPPORTED
     using iter = mapped_iterator<Object, storage_type::db_objects_type>;
+
+    STATIC_REQUIRE(can_iterate_mapped<iter, Object>);
+
     // check default initializability at runtime
     [[maybe_unused]] const iter end;
 #else
