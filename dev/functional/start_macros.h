2e48e44478a21475a427ccc0d05ed4dcd1c0183d
klaus triendl <klaus@triendl.eu>
2025-10-21 14:13:46 +0300

    Ignore annoying clang compiler warnings about using future C++ features
    Clang has the annoying habit of warning about future C++ features that it claims to support through a feature macro.

--- dev/functional/start_macros.h
+++ dev/functional/start_macros.h
@@ -1,5 +1,13 @@
 #pragma once
 
+// Clang has the annoying habit of warning about future C++ features that it claims to support through a feature macro.
+#ifdef __clang__
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Wc++20-extensions"
+#pragma clang diagnostic ignored "-Wc++23-extensions"
+#pragma clang diagnostic ignored "-Wc++26-extensions"
+#endif
+
 #if defined(_MSC_VER)
 __pragma(push_macro("min"))
 #undef min
