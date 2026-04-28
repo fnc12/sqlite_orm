7ce965ab491332aa5a8995c2e49cd3811b2641a5
klaus triendl <klaus@triendl.eu>
2026-04-28 22:15:11 +0300

    Removed code used in local test environment

--- tests/tests.cpp
+++ tests/tests.cpp
@@ -7,14 +7,6 @@
 #include <cstdio>  //  std::remove
 #include <cstring>  //  std::strncmp
 
-#ifdef _DEBUG
-#pragma comment(lib, "Catch2d.Lib")
-//#pragma comment(lib, "manual-link/Catch2Maind.Lib")
-#else
-#pragma comment(lib, "Catch2.Lib")
-//#pragma comment(lib, "manual-link/Catch2Main.Lib")
-#endif
-
 using namespace sqlite_orm;
 
 TEST_CASE("Limits") {
